26ebae8885804178a4762f149638a5aec13efef6
thakkararkil3@gmail.com <thakkararkil1996@gmail.com>
2018-02-07 01:41:46 -0500

    linear regression

---
+++ Linear Regression lab/linearReg.py
@@ -0,0 +1,38 @@
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+dataset = pd.read_csv('Salary_Data.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+
+from sklearn.cross_validation import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size =1/3, random_state = 0)
+
+
+
+
+from sklearn.linear_model import LinearRegression
+abc = LinearRegression()
+abc.fit(x_train,y_train)
+predicty=abc.predict(x_test)
+
+plt.scatter(x_train, y_train,color='red')
+plt.plot(x_train,abc.predict(x_train),color='blue')
+plt.title('salary vs experience')
+plt.xlabel('salary')
+plt.ylabel('experience')
+plt.show()
+
+
+plt.scatter(x_train, y_train,color='red')
+plt.plot(x_train,abc.predict(x_train),color='blue')
+plt.title('salary vs experience')
+plt.xlabel('salary')
+plt.ylabel('experience')
+plt.show()
+
