1d57692683d03fddb09e3d5c8eff057347ee983d
thakkararkil3@gmail.com <thakkararkil1996@gmail.com>
2018-03-12 00:49:31 -0400

    chnages in polyReg

--- Polynomial Regression/polyRegr.R
+++ Polynomial Regression/polyRegr.R
@@ -1,7 +1,9 @@
 dataset = read.csv('Position_Salaries.csv')
 dataset=dataset[2:3]
 lin_reg =lm(formula = Salary ~ .,  data = dataset)
-dataset$Level2 =dataset$Level^4
+dataset$Level2 =dataset$Level^2
+dataset$Level3 =dataset$Level^3
+dataset$Level4 =dataset$Level^4
 poly_reg =lm(formula = Salary ~ .,  data = dataset)
 
 
@@ -17,3 +19,5 @@ ggplot() +
   geom_point(aes(x=dataset$Level , y =dataset$Salary), colour= ' blue' ) +
   geom_line(aes(x=dataset$Level , y = predict(poly_reg ,newdata = dataset)), colour= 'red' ) + 
   ggtitle('poly reg') 
+
+y_pred = predict(poly_reg, data.frame(Level = 6.5,Level2 =6.5^2 , Level3 =6.5^3,Level4 =6.5^4))
