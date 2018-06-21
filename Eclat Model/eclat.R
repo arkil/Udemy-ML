565a0edb0e366a63be06fb0722bcbb27a04de5d4
thakkararkil3@gmail.com <thakkararkil1996@gmail.com>
2018-06-21 02:46:57 -0400

    eclat model for association learninh

---
+++ Eclat Model/eclat.R
@@ -0,0 +1,10 @@
+library(arules)
+
+dataset = read.csv('Market_Basket_Optimisation.csv', header = FALSE)
+dataset = read.transactions('Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE)
+
+itemFrequencyPlot(dataset, topN = 10)
+
+rules = eclat(data = dataset ,parameter = list(support=0.004, minlen = 2))
+
+inspect(sort(rules, by = 'support')[1:10])
